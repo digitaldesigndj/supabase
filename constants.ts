42d95f0621d5262ca33f9cf140f7e17c794945e0
Taylor Y <tdy721@gmail.com>
2023-04-11 09:03:37 -0700

    work

--- constants.ts
+++ constants.ts
@@ -1,9 +1,9 @@
 // Copyright 2023 the Deno authors. All rights reserved. MIT license.
-export const SITE_NAME = "Your SaaS";
-export const SITE_DESCRIPTION = "Some details about your SaaS.";
+export const SITE_NAME = "Hyprtxt";
+export const SITE_DESCRIPTION = "Webmaster for hire.";
 
 export const AUTHENTICATED_REDIRECT_PATH = "/dashboard";
 
 export const STRIPE_PREMIUM_PLAN_PRICE_ID = "price_1MPiEkD6QJts4RaYcp1SevPe";
 
-export const FREE_PLAN_TODOS_LIMIT = 5;
+export const FREE_PLAN_TODOS_LIMIT = 15;
